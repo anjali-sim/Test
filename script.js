f948848909196db23bd563304e907ba61300bd37
Anjali Ghetia <anjalighetia123@gmail.com>
2023-01-17 13:33:54 +0530

    Changes Made

---
+++ script.js
@@ -0,0 +1,11 @@
+function scroll_left()
+{
+    var left = document.querySelector('.left-card')
+    left.scrollBy(350,0)
+}
+
+function scroll_right()
+{
+    var right = document.querySelector('.left-card')
+    right.scrollBy(-350,0)
+}
